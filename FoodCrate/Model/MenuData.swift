1b519bb5271b5810aff2e6ccd6940b4c0528cbc6
Brandon Man <brandonkingyiuman@gmail.com>
2019-04-15 14:02:58 -0700

    Ethan: Update Menu Data Drew: work on account

--- FoodCrate/Model/MenuData.swift
+++ FoodCrate/Model/MenuData.swift
@@ -10,7 +10,10 @@ import Foundation
 
 var PacificHealthFoodsMenuData = [
     FoodItem(name: "Fruit Freeze", price: 7.00, description: "Orange juice, strawberries, banana & blueberries"),
-    FoodItem(name: "Menehune", price: 7.00, description: "Pineapple juice, lime, wheatgrass, spinach, pineapple, mango & banana")
+    FoodItem(name: "Menehune", price: 7.00, description: "Pineapple juice, lime, wheatgrass, spinach, pineapple, mango & banana"),
+    FoodItem(name: "Menehune", price: 7.00, description: "Pineapple juice, lime, wheatgrass, spinach, pineapple, mango & banana"),
+    FoodItem(name: "Coconut Breeze", price: 7.00, description: "Coconut water, pineapple, mango, banana & coconut manna")
+    
 ]
 
 var LuckyLlamaMenuData = [
