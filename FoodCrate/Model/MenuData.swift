bf4ed18af8dad7cb9b2454052a975d8011f465d6
Jack Deardorff <jack_deardorff@cate.org>
2019-04-24 13:44:51 -0700

    I updated the menu.swift with the Pacific Health Foods menu items

--- FoodCrate/Model/MenuData.swift
+++ FoodCrate/Model/MenuData.swift
@@ -13,7 +13,29 @@ var placeholder = [FoodItem(name: "nil", price: 0, description: "nil")]
 var PacificHealthFoodsMenuData = [
     FoodItem(name: "Fruit Freeze", price: 7.00, description: "Orange juice, strawberries, banana & blueberries"),
     FoodItem(name: "Menehune", price: 7.00, description: "Pineapple juice, lime, wheatgrass, spinach, pineapple, mango & banana"),
-    FoodItem(name: "Coconut Breeze", price: 7.00, description: "Coconut water, pineapple, mango, banana & coconut manna")
+    FoodItem(name: "Cinnamon Flax Monkey", price: 7.00, description: "Coconut water, flax/chia honey, cinnamon & peanut butter"),
+    FoodItem(name: "Coconut Breeze", price: 7.00, description: "Coconut water, pineapple, mango, banana & coconut manna"),
+    FoodItem(name: "Qui Gone", price: 7.00, description: "Apple juice, lemon juice, cayenne, ginseng, spinach, mango & pineapple"),
+    FoodItem(name: "Hemp Date", price: 7.00, description: "Coconut water, almond butter, date butter, flax oil & hemp milk"),
+    FoodItem(name: "Mango Tango", price: 7.00, description: "Mango juice, banana, mango & pineapple"),
+    FoodItem(name: "Nutty Carrot", price: 7.00, description: "Carrot juice, peanut butter, cashew milk & honey"),
+    FoodItem(name: "Yo' Blue", price: 7.00, description: "Cashew milk, banana, blueberries & vanila extract"),
+    FoodItem(name: "Lemon Ginger", price: 7.00, description: "Pineapple juice, lemon juice, fresh ginger, mango & banana"),
+    FoodItem(name: "Kauai Breeze", price: 7.00, description: "Pineapple juice, banana & strawberries"),
+    FoodItem(name: "Hawaiian Special", price: 7.00, description: "Coconut water, coconut manna, whey protein, mango, pineapple & strawberries"),
+    FoodItem(name: "Flip Vanilla", price: 7.00, description: "Vanilla whey protein, non-fat milk & ice milk"),
+    FoodItem(name: "Flip Chocolate", price: 7.00, description: "Chocolate whey protein, non-fat milk & ice milk"),
+    FoodItem(name: "Flip Carob", price: 7.00, description: "Carob whey protein, non-fat milk & ice milk"),
+    FoodItem(name: "Flip Mocha", price: 7.00, description: "Mocha whey protein, non-fat milk & ice milk"),
+    FoodItem(name: "Apple Delight", price: 7.00, description: "Apple juice, kale, mango & pineapple"),
+    FoodItem(name: "Monkey Madness", price: 7.00, description: "Non-fat milk, whey protein, ice milk, peanut butter & banana"),
+    FoodItem(name: "The Cold Cure", price: 7.00, description: "Lemon, ginger, clove garlic & dash cayenne"),
+    FoodItem(name: "The Flubber", price: 7.00, description: "Apple juice, frozen pineapple, ginger, basil, lime & spinach"),
+    FoodItem(name: "Chewmacca", price: 7.00, description: "Almond/Cashew milk, frozen hemp milk, maca, cocoa & banana"),
+    FoodItem(name: "Co-Conut", price: 7.00, description: "Coconut water, coconut manna, cocoa powder, banana & icemilk"),
+    FoodItem(name: "Sally's Smoothie", price: 7.00, description: "Acai, apple juice, cashew frozen, strawberries & agave"),
+    FoodItem(name: "Sara's Drink", price: 7.00, description: "Almond milk, cashew milk, cinnamon & date butter"),
+    FoodItem(name: "Veronica's Smoothie", price: 7.00, description: "Cashew frozen, coconut water, banana, peanut butter, date butter, blueberries, spinach & multivitamins"),
     
 ]
 
