bb6b8d75834e5eeeeb3070f5d26c0e9151788d8f
Brandon Man <brandonkingyiuman@gmail.com>
2019-05-15 09:13:02 -0700

    Added DLRadio Button. Need someone to reformat

--- FoodCrate/Control/OrderFoodViewController.swift
+++ FoodCrate/Control/OrderFoodViewController.swift
@@ -8,13 +8,19 @@
 
 import UIKit
 import GoogleSignIn
+import GoogleAPIClientForREST
 import Foundation
-
+import Alamofire
+import DLRadioButton
 
 class OrderFoodViewController: UIViewController {
     var foodSelected = String()
     
     
+    private let scopes = [kGTLRAuthScopeSheetsSpreadsheets]
+    private let service = GTLRSheetsService()
+
+
     @IBOutlet weak var FoodItem: UILabel!
     
     override func viewDidLoad() {
@@ -22,9 +28,30 @@ class OrderFoodViewController: UIViewController {
         
         FoodItem.text = foodSelected
         // Do any additional setup after loading the view.
+        
     }
     
+    
+   
+    @IBOutlet weak var venmo: DLRadioButton!
+    
+    @IBOutlet weak var cash: DLRadioButton!
+    
+    @IBAction func orderFood(_ sender: Any) {
+        if venmo.isSelected == false && cash.isSelected == false{
+            print ("please select a payment method")
+        }else{
+            if venmo.isSelected == true{
+                print("venmo payment method selected")
+            }else{
+                print ("cash payment method selected")
+            }
+            
+        }
+    }
 
+    
+    
     /*
     // MARK: - Navigation
 
--- Podfile
+++ Podfile
@@ -3,5 +3,6 @@ use_frameworks!
 target ‘FoodCrate’ do
     pod 'GoogleAPIClientForREST/Sheets', '~> 1.2.1'
     pod 'Google/SignIn', '~> 3.0.3'
-    pod 'Alamofire'
+    pod 'Alamofire', '~> 4.0'
+    pod 'DLRadioButton', '~> 1.4.12'
 end
